50c3c1de9cf9e1f8b9d08658ec10bdef64b2237d
unknown <crouzicien@gmail.com>
2019-07-27 13:39:09 +0200

    Fix tests

--- test/integration/deleteme.service.test.js
+++ test/integration/deleteme.service.test.js
@@ -6,117 +6,127 @@
  #  Modified By: Sylvain (contact@cashsystemes.eu>)                            #
  #                                                                             #
  #  File Created: Monday, 24th June 2019 10:45:20 am                           #
- #  Last Modified: Friday, 12th July 2019 12:03:12 pm                          #
+ #  Last Modified: Saturday, 27th July 2019 1:36:00 pm                         #
  #                                                                             #
  #  Copyright 2018 - 2019, Cash Systemes Industries                            #
  */
 
- const chai = require('chai');
- const chaiHttp = require('chai-http');
- const should = chai.should();
- chai.use(chaiHttp);
- 
- // console.log(process.env)
- 
- let server
- 
- try {
-     // REGEX for ip in string
-     let r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
-     let app_name = process.env.APP_NAME || 'chaise'
-     let app_ingress_ip = process.env.HUDSON_URL.match(r)[0] || '192.168.30.244';
-     let app_env = process.env.KUBE_ENVIRONMENT || 'jx-staging'
-     server = `http://${app_name}.${app_env}.${app_ingress_ip}.nip.io`
- 
- } catch (error) {
-     server = `http://localhost:3000`
- }
- 
- 
- 
- 
- 
- /*
-  * Test the API
-  */
- describe('TEST DU SERVICE articles', () => {
-     let _id = null
-     let book = {
-         nom: "Sylvain",
-         prenom: "Pip"
-     }
- 
- 
- 
- 
-     it('Ajout d\'un articles', done => {
- 
-         chai.request(server)
-             .post('/api/articles')
-             .send(book)
-             .end((err, res) => {
-                 res.should.have.status(200);
-                 res.body.should.be.a('object');
-                 res.body.should.have.property('nom').eql(book.nom);
-                 res.body.should.have.property('prenom').eql(book.prenom);
-                 res.body.should.have.property('_id').to.be.a('string');
-                 _id = res.body._id
-                 done();
-             });
-     });
- 
- 
- 
- 
-     it('Liste de tout les articles', done => {
-         chai.request(server)
-             .get('/api/articles')
-             .end((err, res) => {
-                 res.should.have.status(200);
-                 res.body.should.be.a('object');
-                 res.body.should.have.property('rows').to.be.a('array');
-                 res.body.should.have.property('total').to.be.a('number');
-                 res.body.should.have.property('page').to.be.a('number');
-                 res.body.should.have.property('pageSize').to.be.a('number');
-                 res.body.should.have.property('totalPages').to.be.a('number');
-                 done();
-             });
-     });
- 
- 
- 
- 
-     it('Recherche du articles', done => {
-         chai.request(server)
-             .get('/api/articles/' + _id)
-             .end((err, res) => {
-                 res.should.have.status(200);
-                 res.body.should.be.a('object');
-                 res.body.should.have.property('nom').eql(book.nom);
-                 res.body.should.have.property('prenom').eql(book.prenom);
-                 res.body.should.have.property('_id').eql(_id)
-                 done();
-             });
-     });
- 
- 
- 
-     it('Suppression du articles', done => {
-         chai.request(server)
-             .delete('/api/articles/' + _id)
-             .end((err, res) => {
-                 res.should.have.status(200);
-                 res.body.should.be.a('object');
-                 res.body.should.have.property('nom').eql(book.nom);
-                 res.body.should.have.property('prenom').eql(book.prenom);
-                 res.body.should.have.property('_id').eql(_id)
-                 done();
-             });
-     });
- 
- 
- 
- 
- 
- 
- });
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+chai.use(chaiHttp);
+
+// console.log(process.env)
+
+let server
+
+try {
+    // REGEX for ip in string
+    let r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
+    let app_name = process.env.APP_NAME || 'chaise'
+    let app_ingress_ip = process.env.HUDSON_URL.match(r)[0] || '192.168.30.244';
+    let app_env = process.env.KUBE_ENVIRONMENT || 'jx-staging'
+    server = `http://${app_name}.${app_env}.${app_ingress_ip}.nip.io`
+
+} catch (error) {
+    server = `http://localhost:3000`
+}
+
+
+
+
+
+/*
+ * Test the API
+ */
+describe('TEST DU CRUD deleteme', () => {
+    let _id = null
+    let data = {
+        champs1: "value1",
+        champs2: "value2"
+    }
+
+
+
+    it('Health method', done => {
+       chai.request(server)
+           .get('/api/deleteme')
+           .end((err, res) => {
+               res.should.have.status(200);
+               res.body.should.be.a('boolean').eql(true)
+               done();
+           });
+   });
+
+
+    it('Create method on Alias', done => {
+
+        chai.request(server)
+            .post('/api/deleteme')
+            .send(data)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('champs1').eql(data.champs1);
+                res.body.should.have.property('champs2').eql(data.champs2);
+                res.body.should.have.property('_id').to.be.a('string');
+                _id = res.body._id
+                done();
+            });
+    });
+
+
+
+
+    it('List method on Alias', done => {
+        chai.request(server)
+            .get('/api/deleteme')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('rows').to.be.a('array');
+                res.body.should.have.property('total').to.be.a('number');
+                res.body.should.have.property('page').to.be.a('number');
+                res.body.should.have.property('pageSize').to.be.a('number');
+                res.body.should.have.property('totalPages').to.be.a('number');
+                done();
+            });
+    });
+
+
+
+
+    it('find method on Alias', done => {
+        chai.request(server)
+            .get('/api/deleteme/' + _id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('champs1').eql(data.champs1);
+                res.body.should.have.property('champs2').eql(data.champs2);
+                res.body.should.have.property('_id').eql(_id)
+                done();
+            });
+    });
+
+
+
+    it('Delete method on Alias', done => {
+        chai.request(server)
+            .delete('/api/deleteme/' + _id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('champs1').eql(data.champs1);
+                res.body.should.have.property('champs2').eql(data.champs2);
+                res.body.should.have.property('_id').eql(_id)
+                done();
+            });
+    });
+
+
+
+
+
+
+});
